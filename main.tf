5216760b7cac7990bd4e2a8f8b293461f68aabb1
David Sandilands <david@puppet.com>
2021-06-16 17:06:59 +0100

    (SOLARCH-352) Inital deploy lets test connection and resource group

---
+++ main.tf
@@ -0,0 +1,99 @@
+# Terraform setup stuff, required providers, where they are sourced from, and
+# the provider's configuration requirements.
+terraform {
+  required_providers {
+    hiera5 = {
+      source  = "sbitio/hiera5"
+      version = "0.2.7"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+# Sets the variables that'll be interpolated to determine where variables are
+# located in the hierarchy
+provider "hiera5" {
+  scope = {
+    architecture = var.architecture
+    replica      = var.replica
+  }
+}
+
+# GCP region and project to operating within
+provider "azure" {
+  features {}
+}
+
+# hiera lookps
+data "hiera5" "server_count" {
+  key = "server_count"
+}
+data "hiera5" "database_count" {
+  key = "database_count"
+}
+data "hiera5_bool" "has_compilers" {
+  key = "has_compilers"
+}
+
+# It is intended that multiple deployments can be launched easily without
+# name collisions
+resource "random_id" "deployment" {
+  byte_length = 3
+}
+
+# 
+#
+resource "azurerm_resource_group" "resource_group" {
+ name     = var.project
+ location = var.region
+}
+
+# Collect some repeated values used by each major component module into one to
+# make them easier to update
+locals {
+  allowed        = concat(["10.128.0.0/9", "35.191.0.0/16", "130.211.0.0/22"], var.firewall_allow)
+  compiler_count = data.hiera5_bool.has_compilers.value ? var.compiler_count : 0
+  id             = random_id.deployment.hex
+  has_lb         = data.hiera5_bool.has_compilers.value ? true : false
+}
+
+# Contain all the networking configuration in a module for readability
+#module "networking" {
+  #source = "./modules/networking"
+  #id     = local.id
+  #allow  = local.allowed
+#}
+
+# Contain all the loadbalancer configuration in a module for readability
+#module "loadbalancer" {
+#  source       = "./modules/loadbalancer"
+#  id           = local.id
+#  ports        = ["8140", "8142"]
+#  region       = var.region
+#  instances    = module.instances.compilers
+#  has_lb       = local.has_lb
+#}
+
+# Contain all the instances configuration in a module for readability
+#module "instances" {
+#  source         = "./modules/instances"
+#  id             = local.id
+#  network        = local.network
+#  subnetwork     = local.subnetwork
+#  user           = var.user
+#  ssh_key        = var.ssh_key
+#  compiler_count = local.compiler_count
+#  node_count     = var.node_count
+#  instance_image = var.instance_image
+#  stack_name     = var.stack_name
+#  project        = var.project
+#  server_count   = data.hiera5.server_count.value
+#  database_count = data.hiera5.database_count.value
+#}
---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "project" {
+  description = "Name of Azure resource group that will be used for grouping infrastructure"
+  type        = string
+}
+variable "user" { 
+  description = "Instance user name that will used for SSH operations"
+  type        = string
+}
+variable "ssh_key" {
+  description = "Location on disk of the SSH public key to be used for instance SSH access"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+variable "region" {
+  description = "Azure region that'll be targeted for infrastructure deployment"
+  type        = string
+  default     = "west-us-2"
+}
+variable "compiler_count" {
+  description = "The quantity of compilers that are deployed behind a load balancer and will be spread across defined zones"
+  type        = number
+  default     = 1
+}
+variable "node_count" {
+  description = "The quantity of nodes that are deployed within the environment for testing"
+  type        = number
+  default     = 0
+}
+variable "instance_image" {
+  description = "The disk image to use when deploying new cloud instances"
+  type        = string
+  default     = "CentOS-based 7.9 - Gen2"
+}
+variable "stack_name" {
+  description = "A name that'll help the user identify which instances are are part of a specific PE deployment"
+  type        = string
+  default     = "puppet-enterprise"
+}
+variable "firewall_allow" {
+  description = "List of permitted IP subnets, list most include the internal network and single addresses must be passed as a /32"
+  type        = list(string)
+  default     = []
+}
+variable "architecture" {
+  description = "Which of the supported PE architectures modules to deploy xlarge, large, or standard"
+  type        = string
+  default     = "standard"
+
+  validation {
+    condition     = contains(["standard", "large", "xlarge"], var.architecture)
+    error_message = "Architecture selection must match one of standard, large, or xlarge."
+  }
+}
+variable "replica" {
+  description = "To deploy instances required for the provisioning of a server replica"
+  type        = bool
+  default     = false
+}
+variable "destroy" {
+  description = "Available to facilitate simplified destroy via Puppet Bolt, irrelevant outside specific use case"
+  type        = bool
+  default     = false
+}
