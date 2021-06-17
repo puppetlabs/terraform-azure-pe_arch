abf2c192d8cdc29613fdaff509b984ef0094263f
David Sandilands <david@puppet.com>
2021-06-17 10:23:07 +0100

    (SOLARCH-352) comment out outputs for now

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 # Output data used by Bolt to do further work, doing this allows for a clean and
 # abstracted interface between cloud provider implementations
-output "console" {
-  value       = module.instances.console
-  description = "This will by the external IP address assigned to the Puppet Enterprise console"
-}
-output "pool" {
-  value       = module.loadbalancer.lb_dns_name
-  description = "The Azure load balncer FQDN of the Puppet Enterprise compiler pool"
-  sensitive   = true
-}
+#output "console" {
+#  value       = module.instances.console
+#  description = "This will by the external IP address assigned to the Puppet Enterprise console"
+#}
+#output "pool" {
+#  value       = module.loadbalancer.lb_dns_name
+#  description = "The Azure load balncer FQDN of the Puppet Enterprise compiler pool"
+#  sensitive   = true
+#}
