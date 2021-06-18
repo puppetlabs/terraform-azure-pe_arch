de98596e90ec7a3cb94d5b21afbc5ddf613206e2
David Sandilands <david@puppet.com>
2021-06-18 10:45:46 +0100

    (SOLARCH-352) added initial networking resources

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -5,12 +5,7 @@ output "virtual_network_id" {
   description = "Virtual network id"
 }
 
-#output "subnet_ids" {
-#  value       = aws_subnet.pe_subnet[*].id
-#  description = "AWS subnet ids"
-#}
-
-#output "security_group_ids" {
-#  value       = aws_security_group.pe_sg[*].id
-#  description = "AWS security group ids"
-#}
+output "subnet_id" {
+  value       = azurerm_subnet.pe_subnet.id
+  description = "subnet ids"
+}
