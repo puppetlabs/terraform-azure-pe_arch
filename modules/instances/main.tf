0d779e171a9226e74d46fc69e03750ae3ed25787
David Sandilands <david@puppet.com>
2021-06-21 10:11:18 +0100

    (SOLARCH-352) adding database instances

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_ssh_public_key" "pe_adm" {
   public_key = file(var.ssh_key)
   resource_group_name = var.resource_group.name
   location = var.region
+  tags = local.name_tag
 }
 
 resource "azurerm_public_ip" "server_public_ip" {
@@ -59,8 +60,6 @@ resource "azurerm_linux_virtual_machine" "server" {
     storage_account_type = "Standard_LRS"
     disk_size_gb         = 50
   }
-  # this should be centos based by rogue wave
-  #CentOS                                              OpenLogic                                           7_9-gen2                                            OpenLogic:CentOS:7_9-gen2:7.9.2021020401 
   source_image_reference {
     publisher = "OpenLogic"
     offer     = "CentOS"
@@ -92,21 +91,47 @@ resource "azurerm_linux_virtual_machine" "server" {
 # large is externalization of the database service. Again given out assumption
 # that extra large currently also means "with replica", we deploy two identical
 # hosts in extra large but nothing in the other two architectures
-#resource "aws_instance" "psql" {
-#  ami                    = data.aws_ami.centos7.id
-#  instance_type          = "t3.2xlarge"
-  # count is used to effectively "no-op" this resource in the event that we
-  # deploy any architecture other than xlarge
-#  count                  = var.database_count
-#  key_name               = aws_key_pair.pe_adm.key_name
-#  subnet_id              = var.subnet_ids[count.index]
-#  tags                   = merge(var.default_tags, tomap({Name = "pe-psql-${var.project}-${count.index}-${var.id}"}))
 
-#  root_block_device {
-#    volume_size = 100
-#    volume_type = "gp2"
-#  }
+resource "azurerm_public_ip" "psql_public_ip" {
+  name                = "pe-psql-${var.project}-${count.index}-${var.id}"
+  resource_group_name = var.resource_group.name
+  location            = var.region
+  count               = var.server_count
+  allocation_method   = "Static"
 
+  tags = local.name_tag
+}
+
+resource "azurerm_linux_virtual_machine" "psql" {
+  name                   = "pe-psql-${var.project}-${count.index}-${var.id}"
+  count                  = var.database_count
+  resource_group_name    = var.resource_group.name
+  location               = var.region
+  size                   = "Standard_D8_v4"
+  admin_username         = var.user
+  network_interface_ids  = [
+    azurerm_network_interface.server_nic[count.index].id,
+  ]
+
+  admin_ssh_key {
+    username   = var.user
+    public_key = azurerm_ssh_public_key.pe_adm.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 100
+  }
+  source_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7_9-gen2"
+    version   = "latest"
+  }
+
+  tags        = local.name_tag
+  
   # Using remote-execs on each instance deployment to ensure things are really
   # really up before doing to the next step, helps with Bolt plans that'll
   # immediately connect then fail
@@ -115,21 +140,24 @@ resource "azurerm_linux_virtual_machine" "server" {
   # to the ssh agent like so:
   # $ eval $(ssh-agent)
   # $ ssh-add
-#  provisioner "remote-exec" {
-#    connection {
-#      host        = self.public_ip
-#      type        = "ssh"
-#      user        = var.user
-#    }
-#    inline = ["# Connected"]
-#  }
-#}
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip_address
+      type        = "ssh"
+      user        = var.user
+    }
+    inline = ["# Connected"]
+  }
+}
 
 # The defining difference between standard and other architectures is the
 # presence of load balanced instances with the sole duty of compiling catalogs
 # for agents. A user chosen number of Compilers will be deployed in large and
 # extra large but only ever zero can be deployed when the operating mode is set
 # to standard
+
+
+
 #resource "aws_instance" "compiler" {
 #  ami                    = data.aws_ami.centos7.id
 #  instance_type          = "t3.xlarge"
