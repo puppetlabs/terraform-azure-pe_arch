7f9707cecbf83e0d34063278bd830b6b07bf6c28
David Sandilands <david@puppet.com>
2021-06-21 10:10:00 +0100

    (SOLARCH-352) correcting versioning error which led to ssh key issue

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.46.0"
+      version = "2.64.0"
     }
     random = {
       source  = "hashicorp/random"
