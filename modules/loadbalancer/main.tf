2670caeee8a1c078ba9e039ddc0f288e5019a4f3
David Sandilands <david@puppet.com>
2021-06-23 15:44:14 +0100

    (SOLARCH-352) Updating variable names had in error kept the same

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -2,7 +2,7 @@ locals {
   lb_count = var.has_lb ? 1 : 0
 }
 
-resource "azurerm_public_ip" "pe_compiler_lb" {
+resource "azurerm_public_ip" "pe_compiler_lb_ip" {
   name                = "pe-compiler-lb-${var.project}-${var.id}"
   count               = local.lb_count
   location            = var.region
@@ -20,13 +20,13 @@ resource "azurerm_lb" "pe_compiler_lb" {
 
   frontend_ip_configuration {
     name                 = "PE-compile-lb-PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.pe_compiler_lb[0].id
+    public_ip_address_id = azurerm_public_ip.pe_compiler_lb_ip[0].id
   }
 }
 
 # This should be the api status check
 # Note default number of probes for failure is 2 and interval is 5 seconds
-resource "azurerm_lb_probe" "pe_compiler_lb" {
+resource "azurerm_lb_probe" "pe_compiler_lb_probe" {
   name                = "PE_compiler_lb_status-${var.project}-${var.id}"
   resource_group_name = var.resourcegroup.name
   loadbalancer_id     = azurerm_lb.pe_compiler_lb[0].id
@@ -35,28 +35,28 @@ resource "azurerm_lb_probe" "pe_compiler_lb" {
   protocol            = "tcp"
 }
 
-resource "azurerm_lb_backend_address_pool" "pe_compiler_lb" {
+resource "azurerm_lb_backend_address_pool" "pe_compiler_lb_backend_pool" {
   loadbalancer_id = azurerm_lb.pe_compiler_lb[0].id
   count           = local.lb_count
   name            = "pe-compiler-lb-${var.project}-${var.id}"
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "pe_compiler_lb" {
+resource "azurerm_network_interface_backend_address_pool_association" "pe_compiler_lb_backend_association" {
   count                   = local.lb_count >= 1 ? length(var.compiler_nics) : 0
   network_interface_id    = var.compiler_nics[count.index].id 
   ip_configuration_name   = "compiler"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.pe_compiler_lb[0].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.pe_compiler_lb_backend_pool[0].id
 }
 
-resource "azurerm_lb_rule" "pe_compiler_lb" {
+resource "azurerm_lb_rule" "pe_compiler_lb_rule" {
   resource_group_name            = var.resourcegroup.name
   loadbalancer_id                = azurerm_lb.pe_compiler_lb[0].id
   count                          = local.lb_count >= 1 ? length(var.ports) : 0
   name                           = var.ports[count.index]
   protocol                       = "Tcp"
   backend_port                   = var.ports[count.index]
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.pe_compiler_lb[0].id
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.pe_compiler_lb_backend_pool[0].id
   frontend_port                  = var.ports[count.index]
   frontend_ip_configuration_name = "PE-compile-lb-PublicIPAddress"
-  probe_id                       = azurerm_lb_probe.pe_compiler_lb[0].id
+  probe_id                       = azurerm_lb_probe.pe_compiler_lb_probe[0].id
 }
--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -1,4 +1,4 @@
 output "lb_dns_name" {
-  value       = var.has_lb ? try(azurerm_public_ip.pe_compiler_lb[0].fqdn, "") : var.primary_nic.fqdn
+  value       = var.has_lb ? try(azurerm_public_ip.pe_compiler_lb_ip[0].fqdn, "") : var.primary_nic.fqdn
   description = "The DNS name of either the load balancer fronting the compiler pool or the primary master, depending on architecture"
 }
