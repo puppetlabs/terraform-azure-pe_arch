e395322f736974ed3f9288d9e769d7b500d60e7b
David Sandilands <david@puppet.com>
2021-06-30 11:21:19 +0100

    (SOLARCH-352) removing use of master

--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -1,4 +1,4 @@
 output "lb_dns_name" {
   value       = var.has_lb ? try(azurerm_public_ip.pe_compiler_lb_ip[0].fqdn, "") : var.primary_ip.fqdn
-  description = "The DNS name of either the load balancer fronting the compiler pool or the primary master, depending on architecture"
+  description = "The DNS name of either the load balancer fronting the compiler pool or the primary, depending on architecture"
 }
