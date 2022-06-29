2468894dd3bb08561ad6da056649c9a7d5eb26c0
Cody Herriges <193064+ody@users.noreply.github.com>
2022-06-29 19:18:30 +0000

    Ensure networking module is consistent
    Updated resource names and tags so they are consistent with rest of
    module

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -1,26 +1,32 @@
 locals {
+  tags     = {
+    description = "PEADM Deployed Puppet Enterprise"
+    project     = var.resourcegroup.name
+  }
   lb_count = var.has_lb ? 1 : 0
 }
 
 resource "azurerm_public_ip" "pe_compiler_lb_ip" {
-  name                = "pe-compiler-lb-${var.project}-${var.id}"
+  name                = "pe-compiler-lb-${var.id}"
   count               = local.lb_count
   location            = var.region
   resource_group_name = var.resourcegroup.name
-  domain_name_label   = "pe-compiler-lb-${var.project}-${var.id}"
+  tags                = local.tags
+  domain_name_label   = "pe-compiler-lb-${var.id}"
   allocation_method   = "Static"
 }
 
 # For simplicity i have made this a standard LB avoiding various issues such as VmIsNotInSameAvailabilitySetAsLb
 # but this should be reviewed more options to allow lab setups to use basic which is free would be desirable
 resource "azurerm_lb" "pe_compiler_lb" {
-  name                = "pe-compiler-lb-${var.project}-${var.id}"
+  name                = "pe-compiler-lb-${var.id}"
   location            = var.region
   count               = local.lb_count
   resource_group_name = var.resourcegroup.name
+  tags                = local.tags
 
   frontend_ip_configuration {
-    name                 = "PE-compile-lb-PublicIPAddress"
+    name                 = "pe-compile-lb-${var.id}"
     public_ip_address_id = azurerm_public_ip.pe_compiler_lb_ip[0].id
   }
 }
@@ -28,7 +34,7 @@ resource "azurerm_lb" "pe_compiler_lb" {
 # This should be the api status check
 # Note default number of probes for failure is 2 and interval is 5 seconds
 resource "azurerm_lb_probe" "pe_compiler_lb_probe" {
-  name                = "PE_compiler_lb_status-${var.project}-${var.id}"
+  name                = "pe-compiler-lb-${var.id}"
   resource_group_name = var.resourcegroup.name
   loadbalancer_id     = azurerm_lb.pe_compiler_lb[0].id
   count               = local.lb_count
@@ -39,7 +45,7 @@ resource "azurerm_lb_probe" "pe_compiler_lb_probe" {
 resource "azurerm_lb_backend_address_pool" "pe_compiler_lb_backend_pool" {
   loadbalancer_id = azurerm_lb.pe_compiler_lb[0].id
   count           = local.lb_count
-  name            = "pe-compiler-lb-${var.project}-${var.id}"
+  name            = "pe-compiler-lb-${var.id}"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "pe_compiler_lb_backend_association" {
@@ -58,6 +64,6 @@ resource "azurerm_lb_rule" "pe_compiler_lb_rule" {
   backend_port                   = var.ports[count.index]
   backend_address_pool_id        = azurerm_lb_backend_address_pool.pe_compiler_lb_backend_pool[0].id
   frontend_port                  = var.ports[count.index]
-  frontend_ip_configuration_name = "PE-compile-lb-PublicIPAddress"
+  frontend_ip_configuration_name = azurerm_lb.pe_compiler_lb[0].frontend_ip_configuration[0].name
   probe_id                       = azurerm_lb_probe.pe_compiler_lb_probe[0].id
 }
