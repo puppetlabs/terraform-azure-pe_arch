0100d3145780eba8e9c96a07ef9c8ad74c61a959
David Sandilands <david.sandilands@perforce.com>
2022-12-21 15:59:30 +0000

    Updating windows node defaults from 1 to 0

--- variables.tf
+++ variables.tf
@@ -38,12 +38,12 @@ variable "node_count" {
 variable "instance_image" {
   description = "The disk image to use when deploying new cloud instances in the form of a full length Image ID or Marketplace URN"
   type        = string
-  default     = "almalinux:almalinux:8-gen2:latest"
+  default     = "openlogic:centos:7_9-gen2:latest"
 }
 variable "image_plan" {
   description = "The Marketplace offering's plan if it has one in Marketplace URN style, name:product:publisher"
   type        = string
-  default     = "8-gen2:almalinux:almalinux"
+  default     = ""
 }
 
 variable "windows_node_count" {
